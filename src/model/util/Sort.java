db32e81eaac0d775d2cd0270f443f66f7ef43c96
ds.najar <ds.najar@uniandes.edu.co>
2019-02-24 23:36:48 -0500

    2

--- src/model/util/Sort.java
+++ src/model/util/Sort.java
@@ -9,24 +9,91 @@ public class Sort {
 	public static void ordenarShellSort( Comparable[ ] datos ) {
 		
 		// TODO implementar el algoritmo ShellSort
+		int tam = datos.length;
+		
+		//Crecimiento de la secuencia
+		int sec = 1;
+		while(sec < tam/3) sec = 3*sec + 1;
+		
+		while(sec >= 1){
+			for(int i = sec; i < tam; i++)
+				for(int j = i; j >= sec && less(datos[j], datos[j-sec]); j -= sec){
+					exchange(datos, j, j-sec);
+				}
+			
+		}
 	}
 	
 	/**
 	 * Ordenar datos aplicando el algoritmo MergeSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
 	 */
-	public static void ordenarMergeSort( Comparable[ ] datos ) {
+	public static void ordenarMergeSort( Comparable[ ] datos, Comparable [ ] aux ) {
 
 		// TODO implementar el algoritmo MergeSort
+		int ult = datos.length;
+		int prim = ult - (ult -1);
+		
+		if(ult <= prim) return;
+		
+		int mid = prim + (ult - prim)/2;
+		
+		for(int i = prim; i<= ult; i++)
+			aux[i] = datos[i];
+		
+		int k = prim, z = mid +1;
+		for(int i = prim; i<= ult; i++)
+			if(k > mid) 			datos[i] = aux[z++];
+			else if(z > ult)		datos[i] = aux[k++];
+			else if(less(aux[z], aux[k]))	datos[i] = aux[z++];
+			else 					datos[i] = aux[k++];
+		
+		
 	}
 
 	/**
 	 * Ordenar datos aplicando el algoritmo QuickSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
 	 */
-	public static void ordenarQuickSort( Comparable[ ] datos ) {
+	public static void ordenarQuickSort( Comparable[ ] datos, int lo, int hi ) {
 
 		// TODO implementar el algoritmo QuickSort
+		int mas = datos.length;
+		int menos = mas - (mas-1);
+		
+		if(mas <= menos) return;
+		int j = particion(datos);
+		
+		ordenarQuickSort(datos, menos, j-1);
+		ordenarQuickSort(datos, j+1, mas);
+		
+	}
+	
+	/**
+	 * Método auxiliar para Quicksort
+	 * @retrun indicador de la partición
+	 */
+	public int particion( Comparable[ ] datos ){
+		int ult = datos.length +1;
+		int prim = ult -(ult - 1);
+		
+		Comparable a = datos[prim];
+		
+		while(true){
+			while(less(datos[++prim], a))
+				if(prim == ult) break;
+			
+			while(less(a, datos[--ult]))
+				if(ult == prim) break;
+			
+			if(prim >= ult) break;
+			
+			exchange(datos, prim, ult);
+		}
+		
+		exchange(datos, prim, ult);
+		
+		return ult;
 	}
 	
 	/**
@@ -38,7 +105,7 @@ public class Sort {
 	private boolean less(Comparable v, Comparable w)
 	{
 		// TODO implementar
-		return false;
+		return (v.compareTo(w) < 0);
 	}
 	
 	/**
@@ -50,6 +117,9 @@ public class Sort {
 	private void exchange( Comparable[] datos, int i, int j)
 	{
 		// TODO implementar
+		Comparable swap = datos[i];
+		datos[i] = datos[j];
+		datos[j] = swap;
 	}
 
 }
